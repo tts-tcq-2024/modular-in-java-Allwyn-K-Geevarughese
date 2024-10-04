117d29aa1df36445f64b46bc84badbf12fa1a924
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-04 17:43:27 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -6,7 +6,7 @@ public class Main {
         RED(1),
         BLACK(2),
         YELLOW(3),
-        VIOLET(4);
+        VIOLET(4); 
         private int index;
         private MajorColor(int index) {
             this.index = index;
