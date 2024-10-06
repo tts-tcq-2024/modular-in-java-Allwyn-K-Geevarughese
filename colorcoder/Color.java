fa6599ff484af33f00ec6c70f88dbca7e6700c1a
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-06 19:59:09 +0530

    Create Color.java

---
+++ colorcoder/Color.java
@@ -0,0 +1,14 @@
+
+package color;
+
+public interface Color {
+    int getIndex();
+    static <E extends Enum<E> & Color> E fromIndex(int index, Class<E> enumClass) {
+        for (E color : enumClass.getEnumConstants()) {
+            if (color.getIndex() == index) {
+                return color;
+            }
+        }
+        return null;
+    }
+}
