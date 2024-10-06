48821aee533fe477b76d3e5ce8b68a345c53fc94
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-06 21:29:09 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -3,32 +3,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class Main {
-
-    static void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
-        ColorPair colorPair = ColorCoderUtils.getColorFromPairNumber(pairNumber);
-        assert(colorPair.getMajor().equals(expectedMajor));
-        assert(colorPair.getMinor().equals(expectedMinor));
-    }
-    static void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
-        assert(ColorCoderUtils.getPairNumberFromColor(major, minor) == expectedPairNumber);
-    }
-
-    static void testPrintColorCodingManual() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        ColorCoderUtils.printColorCodingManual();
-        String expectedOutput = "Color Coding Manual:\n";
-        for (int pairNumber = 1; pairNumber <= ColorCoderUtils.numberOfMajorColors * ColorCoderUtils.numberOfMinorColors; pairNumber++) {
-            expectedOutput += "Pair Number: " + pairNumber + ", Color Pair: " + ColorCoderUtils.getColorFromPairNumber(pairNumber).toString() + "\n";
-        }
-        assert(outContent.toString().equals(expectedOutput));
-    }
+    
     public static void main(String[] args) {
-        testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
-        testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
-        testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
-        testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
-       
+        ColorCoderTest.main(args);
         ColorCoderUtils.printColorCodingManual();
     }
 }
