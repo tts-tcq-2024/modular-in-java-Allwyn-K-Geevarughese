512ce79c356c5b9a7e18b57acfa878cb1c584679
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-06 20:38:49 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -2,17 +2,7 @@ package colorCoder;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 public class Main {
-    final static String MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
-    final static int numberOfMajorColors = MajorColorNames.length;
-    final static String MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
-    final static int numberOfMinorColors = MinorColorNames.length;
-    static ColorPair getColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        return new ColorPair(Color.fromIndex(zeroBasedPairNumber / numberOfMinorColors, MajorColor.class), Color.fromIndex(zeroBasedPairNumber % numberOfMinorColors, MinorColor.class));
-    }
-    static int getPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major.getIndex() * numberOfMinorColors + minor.getIndex() + 1;
-    }
+
     static void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
         ColorPair colorPair = getColorFromPairNumber(pairNumber);
         assert(colorPair.getMajor().equals(expectedMajor));
@@ -20,11 +10,6 @@ public class Main {
     }
     static void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
         assert(getPairNumberFromColor(major, minor) == expectedPairNumber);
-    }
-    static void printColorCodingManual() {
-        for (int pairNumber = 1; pairNumber <= numberOfMajorColors * numberOfMinorColors; pairNumber++) {
-            System.out.println("Pair Number: " + pairNumber + ", Color Pair: " + getColorFromPairNumber(pairNumber).toString());
-        }
     }
 
     static void testPrintColorCodingManual() {
@@ -42,7 +27,7 @@ public class Main {
         testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
         testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
         testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
-        testPrintColorCodingManual();
+        ColorCoderUtils.testPrintColorCodingManual();
         printColorCodingManual();
     }
 }
