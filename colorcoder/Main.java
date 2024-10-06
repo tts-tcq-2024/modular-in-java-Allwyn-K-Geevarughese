2c7d021c9e92826b9cfd2e308a4b2cbf3380b50c
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-06 21:32:33 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -5,7 +5,7 @@ import java.io.PrintStream;
 public class Main {
     
     public static void main(String[] args) {
-        ColorCoderTest.main(args);
+        TestColorCoder.main(args);
         ColorCoderUtils.printColorCodingManual();
     }
 }
