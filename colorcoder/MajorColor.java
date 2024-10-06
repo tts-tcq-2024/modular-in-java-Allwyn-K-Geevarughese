f685c12095d23a8f450059460c3816b88ce6d66a
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-06 19:57:33 +0530

    Create MajorColor.java

---
+++ colorcoder/MajorColor.java
@@ -0,0 +1,18 @@
+
+package color;
+
+public enum MajorColor implements Color {
+    WHITE(0),
+    RED(1),
+    BLACK(2),
+    YELLOW(3),
+    VIOLET(4);
+    private int index;
+    private MajorColor(int index) {
+        this.index = index;
+    }
+    @Override
+    public int getIndex() {
+        return index;
+    }
+}
