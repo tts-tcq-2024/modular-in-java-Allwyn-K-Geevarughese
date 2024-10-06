b57c3bd103d75cb95059c71832de36d4e61eac50
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-06 19:58:47 +0530

    Create ColorPair.java

---
+++ colorcoder/ColorPair.java
@@ -0,0 +1,28 @@
+
+package color;
+
+public class ColorPair {
+    private MajorColor majorColor;
+    private MinorColor minorColor;
+
+    public ColorPair(MajorColor major, MinorColor minor) {
+        this.majorColor = major;
+        this.minorColor = minor;
+    }
+
+    public MajorColor getMajor() {
+        return majorColor;
+    }
+
+    public MinorColor getMinor() {
+        return minorColor;
+    }
+
+    @Override
+    public String toString() {
+        String colorPairStr = Main.MajorColorNames[majorColor.getIndex()];
+        colorPairStr += " ";
+        colorPairStr += Main.MinorColorNames[minorColor.getIndex()];
+        return colorPairStr;
+    }
+}
