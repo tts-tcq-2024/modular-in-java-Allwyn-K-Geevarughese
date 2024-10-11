4718f9b8557b85c00ec67575c5dfb9f4ed04e21e
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-11 16:25:37 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -5,7 +5,11 @@ import java.io.PrintStream;
 public class Main {
     
     public static void main(String[] args) {
-        TestColorCoder.main(args);
+        
+        TestColorCoder.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+        TestColorCoder.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+        TestColorCoder.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+        TestColorCoder.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
         ColorCoderUtils.printColorCodingManual();
     }
 }
