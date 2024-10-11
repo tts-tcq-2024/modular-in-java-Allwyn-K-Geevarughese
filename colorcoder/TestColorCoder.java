ae42300d6f970149a3edabfad92463cac969bfe6
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-10-11 16:24:35 +0530

    Update TestColorCoder.java

--- colorcoder/TestColorCoder.java
+++ colorcoder/TestColorCoder.java
@@ -17,11 +17,5 @@ public class TestColorCoder {
         assert (pairNumber == expectedPairNumber);
     }
 
-    public static void main(String[] args) {
-        testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
-        testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
-
-        testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
-        testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
-    }
+   
 }
